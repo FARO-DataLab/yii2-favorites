2c923e15eda3054481d4e446f903c3c0f069504f
Gonzalo Bourdieu <gonzalobourdieu@gmail.com>
2022-04-21 13:49:17 -0300

    Fix en las tablas

--- migrations/m161115_084412_init_favorites.php
+++ migrations/m161115_084412_init_favorites.php
@@ -1,6 +1,9 @@
 <?php
 
+namespace faro\core\favorites\migrations;
+
 use faro\core\components\FaroBaseMigration;
+use Yii;
 use yii\db\Migration;
 use yii\db\Schema;
 
@@ -12,18 +15,18 @@ class m161115_084412_init_favorites extends FaroBaseMigration
     public function up()
     {
         $tableOptions = '';
-
-        if (Yii::$app->db->driverName == 'mysql')
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
-
+        
         $this->crearTabla('{{%core_favorito}}', [
             'id'                   => Schema::TYPE_PK,
+            'created_by'           => Schema::TYPE_INTEGER,
+            'updated_by'           => Schema::TYPE_INTEGER,
             'model'                => Schema::TYPE_TEXT,
             'target_id'            => Schema::TYPE_TEXT, // can be a slug, not only an numeric id
             'target_attribute'     => Schema::TYPE_TEXT,
             'route'                => Schema::TYPE_TEXT,
             'url'                  => Schema::TYPE_TEXT,
             'icon'                 => Schema::TYPE_STRING,
+
         ], $tableOptions);
     }
 
