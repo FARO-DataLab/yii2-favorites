20eef4f54da9f83f752aff1263a4ddb44a749057
gonzab <gonzalo@bassobrovelli.com>
2022-09-07 10:42:05 +0000

    Merge pull request #29 from FARO-DataLab/feature/v2022.1.3-listado-cuentas-gonza

--- views/favorites/index.php
+++ views/favorites/index.php
@@ -1,9 +1,12 @@
 <?php
 
 use faro\core\components\FaroGridView;
+use faro\core\components\HtmlComponentsHelper;
+use faro\core\widgets\Panel;
 use yii\grid\GridView;
 use yii\helpers\Html;
 use yii\helpers\Url;
+use yii\helpers\VarDumper;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\SitecontentSearch */
@@ -14,29 +17,29 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="favorites-index">
 
+    <?php Panel::begin(["header" => false]) ?>
 
     <?= FaroGridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => null,
+        'layout' => "{items}{pager}",
         'columns' => [
             [
                 'filter' => false,
-                'format' => 'html',
-                'attribute' => 'icon',
-                'header' => '&nbsp;',
-                'value' => function ($data) {
-                    if ($data->icon) {
-                        return $data->icon;
-                    }
-                }
-            ],
-            [
-                'filter' => false,
                 'format' => 'raw',
-                'attribute' => 'target_id',
+                'label' => "Categoria",
                 'value' => function ($data) {
                     if ($data->target) {
-                        return Html::a($data->target->nombre, $data->url, ['data-pjax' => 0]);
+                        $categoria = $data->target->getCategoria()->one();
+                        if (!$categoria) {
+                            return "-";
+                        }
+
+                        return HtmlComponentsHelper::imprimirBadgeCategoria(
+                            $categoria->nombre,
+                            $categoria->slug,
+                            $categoria->color
+                        );
                     }
                 }
             ],
@@ -45,20 +48,38 @@ $this->params['breadcrumbs'][] = $this->title;
                 'format' => 'raw',
                 'attribute' => 'model',
                 "header" => "Tipo",
+                "contentOptions" => ["style" => "max-width: 150px;"],
                 'value' => function ($data) {
                     if ($data->model) {
                         $aliases = Yii::$app->getModule('favorites')->modelAliases;
 
-                        return isset($aliases[$data->model]) ? Yii::t('app',
-                            $aliases[$data->model]) : $data->model;
+                        return isset($aliases[$data->model])
+                            ? Yii::t('app', $aliases[$data->model])
+                            : Yii::t('app', $data->model);
                     }
                 }
             ],
+
+            [
+                'filter' => false,
+                'format' => 'raw',
+                'attribute' => 'target_id',
+                'label' => "Objeto",
+                'value' => function ($data) {
+                    if ($data->target) {
+                        return Html::a($data->target->nombre, $data->url, ['data-pjax' => 0]);
+                    }
+                }
+            ],
+
             [
                 'attribute' => 'fecha_ingreso_sistema',
                 'filter' => false,
+                'label' => 'Creado',
                 'format' => 'relativeTime',
             ],
         ],
     ]); ?>
+
+    <?php Panel::end() ?>
 </div>
